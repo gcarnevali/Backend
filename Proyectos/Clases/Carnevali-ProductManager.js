49f60a5049c57df5de9ec9cff547bd308b0a4a8c
gcarnevali <carnevaligianni0@gmail.com>
2023-08-12 18:51:50 -0300

    2da entrega

--- Proyectos/Clases/Carnevali-ProductManager.js
+++ Proyectos/Clases/Carnevali-ProductManager.js
@@ -7,12 +7,15 @@ class ProductManager {
         this.path = path;
     }
 
+//MÉTODO PARA OBTENER PRODUCTOS------------------------------------------------------------------------------------------
+
     async getProducts() {
         if (fs.existsSync(this.path)) {
             return JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
         } else return []
     }
-    //TODO: Agregar un metodo para agregar el producto al array de products
+
+//MÉTODO PARA AÑADIR PRODUCTOS-------------------------------------------------------------------------------------------
 
     async addProduct(title, description, price, thumbnail, code, stock) {
         let newProduct = { title, description, price, thumbnail, code, stock }
@@ -27,20 +30,82 @@ class ProductManager {
         await fs.promises.writeFile(this.path, JSON.stringify(this.products))
     }
 
+//MÉTODO PARA OBTENER PRODUCTOS POR ID-----------------------------------------------------------------------------------
 
-    getProductsById(idProduct) {
-        let indiceProducto = this.products.findIndex(product => product.id === idProduct)
-
-        if (indiceProducto === -1) {
-            console.log(`El producto ${idProduct} no existe`)
-            return
+    async getProductsById(idProduct) {
+        if (!Number.isInteger(idProduct)) {
+            console.log(`El ID del producto debe ser un número entero.`);
+            return null;
         }
 
-        return this.products[indiceProducto]
+        if (fs.existsSync(this.path)) {
+            const productsData = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'));
+            const foundProduct = productsData.find(product => product.id === idProduct);
+
+            if (foundProduct) {
+                console.log(`Producto encontrado con ID ${idProduct}.`);
+                return foundProduct; // Devolver el objeto completo del producto encontrado
+            } else {
+                console.log(`No se encontró ningún producto con ID ${idProduct}.`);
+                return null;
+            }
+        } else {
+            console.log(`No existe el archivo de productos.`);
+            return null;
+        }
+    }
+
+//MÉTODO PARA ACTUALIZAR PRODUCTOS---------------------------------------------------------------------------------------
+
+    async updateProducts(idProduct, newStock) {
+        let indexToUpdate = this.products.findIndex(product => product.id === idProduct);
+
+        if (indexToUpdate === -1) {
+            console.log(`El producto con ID ${idProduct} no existe.`);
+            return;
+        }
+
+        this.products[indexToUpdate].stock = newStock;
+        await this.saveProductsToFile(); // Llamamos al método para guardar productos en el archivo
+        console.log(`Stock del producto con ID ${idProduct} actualizado a ${newStock}.`);
+    }
+
+    async saveProductsToFile() {
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+            console.log('Productos guardados en el archivo.');
+        } catch (error) {
+            console.error('Error al guardar los productos en el archivo:', error);
+        }
+    }
+
+//MÉTODO PARA BORRAR PRODUCTOS-------------------------------------------------------------------------------------------
+
+    async deleteProduct(idProduct) {
+        if (!Number.isInteger(idProduct)) {
+            console.log(`El ID del producto debe ser un número entero.`);
+            return;
+        }
+
+        if (fs.existsSync(this.path)) {
+            const productsData = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'));
+            const productIndex = productsData.findIndex(product => product.id === idProduct);
+
+            if (productIndex !== -1) {
+                productsData.splice(productIndex, 1);
+                await fs.promises.writeFile(this.path, JSON.stringify(productsData, null, 2));
+                console.log(`Producto con ID ${idProduct} eliminado.`);
+            } else {
+                console.log(`No se encontró ningún producto con ID ${idProduct}.`);
+            }
+        } else {
+            console.log(`No existe el archivo de productos.`);
+        }
     }
 
 }
 
+//VARIABLE PARA INSTANCIAR DE MANERA ASÍNCRONA AL PROGRAMA---------------------------------------------------------------
 const entorno = async () => {
     let path = './archivos/products.json'
     const manager = new ProductManager(path);
@@ -48,30 +113,17 @@ const entorno = async () => {
     await manager.addProduct('Producto1', 'Lorem', 100, '/img', 1, 5)
     await manager.addProduct('Producto2', 'Lorem Ipsum ', 150, '/img2', 2, 5)
 
-    console.log(await manager.getProducts())
+    await manager.getProducts()
 
-    console.log(manager.getProductsById(1))
-    manager.getProductsById(3)
+    //await manager.getProductsById(1)
+    //manager.getProductsById(3)
+
+    //manager.updateProducts(1, 10)
+    //manager.getProductsById(1)
+
+    //await manager.deleteProduct(2)
 
 }
 
+//EJECUCIÓN DEL PROGRAMA-------------------------------------------------------------------------------------------------
 entorno()
-
-
-
-
-
-/*getProductsById(idProduct) {
-        try {
-            const products = JSON.parse(data)
-            const product = products.find(p => p.id === idProduct)
-
-            if(product){
-                return (idProduct)
-            } else {
-                return ('Producto no encontrado')
-            }
-        } catch (parseError) {
-            return (parseError)
-        }
-    }*/
